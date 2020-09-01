dfc18f9789f2a00f5128e1a6dba06026ac5b879a
SirPaws <jarccc7788@gmail.com>
2020-09-01 22:02:00 +0200

    removed language check from pstring.h

--- include/pstring.h
+++ include/pstring.h
@@ -3,8 +3,6 @@
 #define PSTD_PSTRING_HEADER
 #include "general.h"
 
-#if PLANG_C
-
 #define pCreateString(str) (String){ .c_str = (u8 *)(str), .length = sizeof((str)) - 1 }
 
 typedef struct String String;
@@ -26,23 +24,4 @@ static String pString(u8 *c_str, usize length) {
 }
 // void pStringStreamPush(struct StringStream *ss, const char *str);
 // void pStringStreamPushf(struct StringStream *ss, const char *fmt, ...);
-#endif
-
-#if PLANG_CPP
-#include <string.h>
-
-struct String {
-	const char* c_str = nullptr;
-	u64 length = 0;
-	String(const char* string, u64 length) :c_str(string), length(length) {}
-	template<u64 count>
-	constexpr String(const char(&string)[count]) : c_str(string), length(count - 1) {}
-
-	String() {}
-	bool operator== (const String& other) {
-		if (length == other.length) return strncmp(c_str, other.c_str, length) == 0;
-		return false;
-	}
-};
-#endif
 #endif // PSTD_PSTRING_HEADER
