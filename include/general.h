03d2d433e2afbe516cf40ac277d392261f832045
SirPaws <jarccc7788@gmail.com>
2022-07-26 11:18:38 +0200

    [general.h] added PSTD_DEFAULT macro and made language versions easier to work with

--- include/general.h
+++ include/general.h
@@ -47,21 +47,25 @@
 
 #if defined(__STDC_VERSION__)
 #   if __STDC_VERSION__ == 199901
-#      define PSTD_C99 1
+#      define PSTD_C99 2
 #      define PSTD_C89 1
+#      define PSTD_C_VERSION PSTD_C99
 #   elif __STDC_VERSION__ == 201112
-#      define PSTD_C11 1
-#      define PSTD_C99 1
+#      define PSTD_C11 3
+#      define PSTD_C99 2
 #      define PSTD_C89 1
+#      define PSTD_C_VERSION PSTD_C11
 #   elif __STDC_VERSION__ == 201710
-#      define PSTD_C18 1
-#      define PSTD_C17 1
-#      define PSTD_C11 1
-#      define PSTD_C99 1
+#      define PSTD_C18 4
+#      define PSTD_C17 4
+#      define PSTD_C11 3
+#      define PSTD_C99 2
 #      define PSTD_C89 1
+#      define PSTD_C_VERSION PSTD_C18
 #   endif
 #elif defined(__STDC__)
 #       define PSTD_C89 1
+#      define PSTD_C_VERSION PSTD_C89
 #endif
 
 #if defined(__STDC_NO_VLA__)
@@ -75,7 +79,9 @@
 #if defined(__has_c_attribute)
 #   define PSTD_HAS_ATTRIBUTE __has_c_attribute
 #   if __STDC_VERSION__ >= 201710 
-#       define PSTD_C23 1
+#       define PSTD_C23 5
+#       undef  PSTD_C_VERSION
+#       define PSTD_C_VERSION PSTD_C23
 #   endif 
 #else
 #   define PSTD_HAS_ATTRIBUTE(x) 0
@@ -90,16 +96,48 @@
 #define PSTD_UNUSED
 #endif
 
-#if defined(PSTD_MSVC) && (defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL)
-#pragma message("Warning: the traditional msvc preprocessor does not support 'complicated' macros use /Zc:preprocessor") 
-#define PSTD_HAS_SECOND(...) 0
-#else
-#define PSTD_HAS_SECOND_TEST__(_0, _1, _2, ...) _2
-#define PSTD_HAS_SECOND_TRIGGER(...) ,
-#define PSTD_HAS_SECOND_TEST_(...) PSTD_HAS_SECOND_TEST__(__VA_ARGS__)
-#define PSTD_HAS_SECOND_TEST(...)  PSTD_HAS_SECOND_TEST_(PSTD_HAS_SECOND_TRIGGER __VA_ARGS__ (), 0, 1, 0) 
-#define PSTD_HAS_SECOND(a, ...)    PSTD_HAS_SECOND_TEST(__VA_ARGS__)
-#endif
+#define PSTD_ARG16(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, ...) _15
+#define PSTD_HAS_COMMA(...) PSTD_ARG16(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0)
+#define PSTD_TRIGGER_PARENTHESIS_(...) ,
+#define PSTD_REMOVE_EMPTY(...) ,,,,,,,,,,,,,,,
+#define PSTD_INDIRECT1(tuple) PSTD_ARG16 tuple 
+#define PSTD_INDIRECT0(...)  PSTD_INDIRECT1((PSTD_REMOVE_EMPTY __VA_ARGS__ () 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+#define PSTD_INDIRECT(...) PSTD_INDIRECT0(__VA_ARGS__)
+
+// based off of an implementation by Jens Gusted https://gustedt.wordpress.com/2010/06/08/detect-empty-macro-arguments/ (may, 2022)
+#define PSTD_ISEMPTY(...)                                                   \
+PSTD_ISEMPTY_(                                                              \
+          /* test if there is just one argument, eventually an empty        \
+             one */                                                         \
+          PSTD_HAS_COMMA(__VA_ARGS__),                                      \
+          /* test if _TRIGGER_PARENTHESIS_ together with the argument       \
+             adds a comma */                                                \
+          PSTD_HAS_COMMA(PSTD_TRIGGER_PARENTHESIS_ __VA_ARGS__),            \
+          /* test if the argument together with a parenthesis               \
+             adds a comma */                                                \
+          PSTD_HAS_COMMA(__VA_ARGS__ (/*empty*/)),                          \
+          /* test if placing it between _TRIGGER_PARENTHESIS_ and the       \
+             parenthesis adds a comma */                                    \
+          PSTD_HAS_COMMA(PSTD_TRIGGER_PARENTHESIS_ __VA_ARGS__ (/*empty*/)),\
+          PSTD_INDIRECT(__VA_ARGS__)                                        \
+          )
+
+
+#define PSTD_CONCAT_5(_0, _1, _2, _3, _4) _0 ## _1 ## _2 ## _3 ## _4
+#define PSTD_ISEMPTY_CONCAT(_0, _1, _2, _3, _4) PSTD_CONCAT_5(_0, _1, _2, _3, _4)
+#define PSTD_ISEMPTY_TUPLE2(...)  PSTD_ISEMPTY_CONCAT __VA_ARGS__
+#define PSTD_ISEMPTY_TUPLE1(a, b) PSTD_CONCAT(PSTD_IS_EMPTY_CASE_, b)
+#define PSTD_ISEMPTY_TUPLE0(...)  PSTD_ISEMPTY_TUPLE1 __VA_ARGS__
+#define PSTD_ISEMPTY_(...)  PSTD_ISEMPTY_TUPLE0((a, PSTD_ISEMPTY_TUPLE2 ((__VA_ARGS__))))
+#define PSTD_IS_EMPTY_CASE_00001 1
+#define PSTD_IS_EMPTY_CASE_00011 1
+#define PSTD_IS_EMPTY_CASE_00011 1
+#define PSTD_IS_EMPTY_CASE_00000 0
+#define PSTD_IS_EMPTY_CASE_11110 0
+#define PSTD_IS_EMPTY_CASE_11110 0
+#define PSTD_IS_EMPTY_CASE_11110 0
+#define PSTD_IS_EMPTY_CASE_11110 0
+
 
 #define PSTD_CONCAT_( a, b ) a##b
 #define PSTD_CONCAT( a, b ) PSTD_CONCAT_( a, b )
@@ -107,9 +145,15 @@
 #define PSTD_STRINGIFY_(x) #x
 #define PSTD_STRINGIFY(x) PSTD_STRINGIFY_(x)
 
-#ifndef PSTD_CASE
-#define PSTD_CASE CAMEL
-#endif
+#define PSTD_DEFAULT_1(actual, default) default
+#define PSTD_DEFAULT_0(actual, default) actual
+
+#define PSTD_DEFAULT_(...) PSTD_CONCAT __VA_ARGS__
+#define PSTD_DEFAULT(value, default) PSTD_DEFAULT_((PSTD_DEFAULT_, PSTD_ISEMPTY(value)))(value, default)
+
+_Static_assert(PSTD_DEFAULT(, 5)  == 5, "");
+_Static_assert(PSTD_DEFAULT(2, 5) == 2, "");
+
 
 
 #include <stdio.h>
@@ -149,9 +193,10 @@ typedef float     f32;
 typedef double    f64;
 
 #if !defined(__cplusplus)
-#   if defined(PSTD_C99)
-        enum { false, true };
+#   if PSTD_C_VERSION >= PSTD_C99
         typedef _Bool pbool_t;
+        static const pbool_t false = 0;
+        static const pbool_t true  = 1;
 #   else
         enum pbool_t { false, true };
         typedef enum pbool_t pbool_t;
@@ -160,13 +205,29 @@ typedef double    f64;
     using p_bool = pbool_t;
 #endif
 
+#if defined(PSTD_MSVC) || (__clang__ && _WIN32)
+#   define PSTD_FORCE_INLINE __forceinline
+#else
+#   define PSTD_FORCE_INLINE inline __attribute__((always_inline))
+#endif
+
 #if defined(PSTD_GNU_COMPATIBLE)
-#define PSTD_ASSUME(x) __builtin_assume((x))
-#define PSTD_EXPECT(cond, expected_result) __builtin_expect((cond), (expected_result))
+#   define PSTD_ASSUME(x) __builtin_assume((x))
+#   define PSTD_EXPECT(cond, expected_result) __builtin_expect((cond), (expected_result))
+#   if PSTD_C23
+#       define PSTD_NONNULL [[gnu::nonnull]]
+#   else
+#       define PSTD_NONNULL __attribute__((nonnull))
+#   endif
 #else
 #define PSTD_ASSUME(x) __assume((x))
 #define PSTD_EXPECT(cond, expected_result) (cond)
 #define register __register
+#   if PSTD_C23
+#       define PSTD_NONNULL [[non_null]]
+#   else
+#       define PSTD_NONNULL 
+#   endif
 #endif
 
 #if defined(PSTD_GNU_COMPATIBLE)
