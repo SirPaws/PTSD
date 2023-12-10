b10e4d762da24eef63b55c6fe629c40bc8ccb77e
SirPaws <jarccc7788@gmail.com>
2023-12-11 00:59:04 +0100

    updated spelling in window example

--- examples/window.c
+++ examples/window.c
@@ -7,7 +7,7 @@ int main(void) {
 
     const pwindow_t *win = pwindow(&(pwindow_info_t){
         pdevices((void*)&keyboard, (void*)&mouse),
-        phints(PHINT_UNDECORATION, PHINT_FULLSCREEN),
+        phints(PHINT_UNDECORATED, PHINT_FULLSCREEN),
         .width = 1024,
     });
 
@@ -15,7 +15,7 @@ int main(void) {
         ppoll_events(win);
 
         pprintf("\x1b[s");
-        pprintf("(%f, %f)\n", mouse.inputs[PSTD_MOUSE_XY].x, mouse.inputs[PSTD_MOUSE_XY].y);
+        pprintf("(%f, %f)\n", mouse.inputs[PTSD_MOUSE_XY].x, mouse.inputs[PTSD_MOUSE_XY].y);
         pprintf("W: (pressed: %B, released: %B, held: %B)                 \n", keyboard.keys['W'].pressed, keyboard.keys['W'].released, keyboard.keys['W'].held);
         pprintf("A: (pressed: %B, released: %B, held: %B)                 \n", keyboard.keys['A'].pressed, keyboard.keys['A'].released, keyboard.keys['A'].held);
         pprintf("S: (pressed: %B, released: %B, held: %B)                 \n", keyboard.keys['S'].pressed, keyboard.keys['S'].released, keyboard.keys['S'].held);
