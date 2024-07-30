5b038c9d00b164c7f94a8c75698c593fb0b9f425
SirPaws <jarccc7788@gmail.com>
2024-07-30 13:11:50 +0200

    added comments to util.h

--- include/util.h
+++ include/util.h
@@ -24,9 +24,15 @@ static bool pcmp_range_i(s32 num, s32 start, s32 end) {
     return (num >= start) && (num <= end);
 }
 
+// on windows this will return STD_OUTPUT_HANDLE, on all other platforms it does nothing
 void *pget_platform_handle(void);
 
-// a version of memset but for bigger data types see tests for more info
+// a version of memset but for bigger data types
+// this will insert `src` as many times as it'll fit inside of `dst`.
+// 
+// if dst cannot store the full `src` it'll store as much of it as there is room for.
+// for example if you tried to store a 32 bit integer in an 8 bit integer
+// it would only store the first byte
 void *pmemexpand(void *dst, void *src, usize dstsize, usize srcsize);
 
 #if defined(__cplusplus)
