ddd36d0a2c90efacd4145621a2876ad081eb6d3e
SirPaws <jarccc7788@gmail.com>
2025-06-22 14:11:18 +0200

    reworked pcli to be useful, still a work in progress

--- include/pcli.h
+++ include/pcli.h
@@ -4,66 +4,83 @@
 #include "pstring.h"
 #include "stretchy_buffer.h"
 
-struct pcli_t;
-typedef bool pcli_arg_handler(struct pcli_t*, void *userdata, pstring_t value);
+
+enum pcli_command_data_t : usize {
+    PCLI_DATA_NONE, 
+    PCLI_DATA_TOGGLE, 
+    PCLI_DATA_STRING,   // pstring_t
+    PCLI_DATA_STRING_C, // const char *
+    PCLI_DATA_NUMBER, 
+    PCLI_DATA_INT
+};
+typedef enum pcli_command_data_t pcli_command_data_t;
+
+typedef struct pcli_command_t pcli_command_t;
+struct pcli_command_t {
+    pcli_command_t *sub_commands;
+    pcli_command_t *options; // for stuff like flags if you have EXE COMMAND --some_flag 
+    usize hash;
+    pstring_t name;
+    usize id;
+    pstring_t *stretchy aliases;
+    pcli_command_data_t *stretchy data;
+};
+
+typedef struct pcli_command_name_map_node_t pcli_command_name_map_node_t;
+struct pcli_command_name_map_node_t {
+    pcli_command_name_map_node_t *next;
+    usize hash;
+    pstring_t name;
+    pcli_command_t *value;
+};
+
+typedef struct pcli_command_name_map_t pcli_command_name_map_t;
+struct pcli_command_name_map_t {
+    usize num_elements;
+    pcli_command_name_map_node_t *buckets[32];
+};
+
+typedef struct pcli_command_id_map_node_t pcli_command_id_map_node_t;
+struct pcli_command_id_map_node_t {
+    pcli_command_id_map_node_t *next;
+    usize hash;
+    usize id;
+    pcli_command_t *value;
+};
+
+typedef struct pcli_command_id_map_t pcli_command_id_map_t;
+struct pcli_command_id_map_t {
+    usize num_elements;
+    pcli_command_id_map_node_t *buckets[32];
+};
 
 typedef struct pcli_t pcli_t;
 struct pcli_t {
-    struct pcli_internal_argument_t {
-        pstring_t spelling;
-        int kind;
-        // for each kind
-        pcli_arg_handler *handlers[5];
-    } *stretchy arguments;
-    void *userdata;
+    pcli_command_name_map_t name_map;
+    pcli_command_id_map_t   id_map;
+    pcli_command_t *stretchy commands;
+    usize max_data_size;
+    bool has_basecommand;
+    bool has_help;
+    bool has_version;
+    bool has_short_version;
+    usize basecommand_id;
+    usize help_id;
+    usize version_id;
+    usize short_version_id;
+    u8 *data_buffer;
 };
 
-typedef struct pcli_option_t pcli_option_t;
-struct pcli_option_t {
-    enum pcli_argument_kind_t {
-        PCLI_INVALID,
-        PCLI_TOGGLE = (1 << 0),
-        PCLI_EQUALS = (1 << 1), // allows '-I=path' where '-I' is the argument spelling
-        PCLI_AFTER  = (1 << 2), // allows '-Ipath'  where '-I' is the argument spelling
-        PCLI_NEXT   = (1 << 3), // allows '-I path' where '-I' is the argument spelling
-        PCLI_MS     = (1 << 4), // allows '/I:path' where '/I' is the argument spelling
-    } kind;
-    pstring_t spelling;
-    pcli_arg_handler *handler;
-    const char *info; // info about parameter
-    const char *verbose_info;
-};
-typedef pcli_option_t pcli_opt_t;
+//TODO(Paw): add flags and shit
 
-#define pcli_create_opt(_kind, _spelling, _handler, ...)\
-    (pcli_opt_t){\
-        .kind=(_kind), \
-        .spelling=pcreate_const_string(#_spelling), \
-        .handler=(_handler)\
-        __VA_ARGS__\
-    }
+void pcli_push_command(pcli_t ctx[static const 1], pstring_t name, usize id);
+void pcli_push_command_data(pcli_t ctx[static const 1], usize id, pcli_command_data_t);
+void pcli_set_basecommand(pcli_t ctx[static const 1], usize id);
+void pcli_set_help(pcli_t ctx[static const 1], usize id);
+void pcli_set_version(pcli_t ctx[static const 1], usize id);
+void pcli_set_short_version(pcli_t ctx[static const 1], usize id);
 
-#define pcli_toggle(_spelling, _handler, ...) \
-    pcli_create_opt(PCLI_TOGGLE,_spelling, _handler, __VA_ARGS__)
-#define pcli_equals(_spelling, _handler, ...) \
-    pcli_create_opt(PCLI_EQUALS,_spelling, _handler, __VA_ARGS__)
-#define pcli_after(_spelling, _handler, ...)  \
-    pcli_create_opt(PCLI_AFTER, _spelling, _handler, __VA_ARGS__)
-#define pcli_next(_spelling, _handler, ...)   \
-    pcli_create_opt(PCLI_NEXT,  _spelling, _handler, __VA_ARGS__)
-#define pcli_ms(_spelling, _handler, ...)     \
-    pcli_create_opt(PCLI_MS,    _spelling, _handler, __VA_ARGS__)
-
-#define pcli_init(opt, userdata)\
-    pcli_init_implementation(countof(opt), opt, userdata)
-
-pcli_t pcli_init_implementation(usize count, const pcli_opt_t opt[count], void *userdata);
-
-void pcli_show_help(pcli_t *, int (*printer)(const char*, ...));
-void pcli_show_verbose_help(pcli_t *, pstring_t argument, int (*printer)(const char*, ...));
-
-void pcli_run(pcli_t *, int, const char*[]);
-
+void *pcli_run(pcli_t ctx[static const 1], int argc, char *argv[]);
 void pcli_free(pcli_t *);
 
 #endif // PTSD_CLI_HEADER 
--- src/pcli.c
+++ src/pcli.c
@@ -1,129 +1,376 @@
 #include "pcli.h"
+#include "hash.h"
+#include "pstacktrace.h"
 
-#define PCLI_TOGGLE_INDEX (0)
-#define PCLI_EQUALS_INDEX (1)
-#define PCLI_AFTER_INDEX  (2)
-#define PCLI_NEXT_INDEX   (3)
-#define PCLI_MS_INDEX     (4)
+struct PCLI_GLOBALS {
+    union { 
+        char buf[sizeof(usize)];
+        usize seed;
+    };
+    size_t argc;
+    char **argv;
+} PCLI_GLOBALS = {
+    .buf = "clicli\0"
+};
 
-// -1 is not found
-isize pcli_has_spelling(pcli_t *, pstring_t);
+void pcli_push_name_to_map(pcli_command_name_map_t map[static 1], usize hash, pstring_t name, pcli_command_t *command) {
+    map->num_elements++;
+    auto node = map->buckets[hash%32];
+    pcli_command_name_map_node_t *new_node = pallocate(sizeof *node);
+    *new_node = (pcli_command_name_map_node_t){};
+    new_node->name = name;
+    new_node->hash = hash;
+    new_node->value = command;
+    if (node) {
+        while (node->next) node = node->next;
+        node->next = new_node;
+    } else {
+        map->buckets[hash%32] = new_node;
+    }
+}
 
-pcli_t pcli_init_implementation(usize count, const pcli_opt_t opt[count], void *userdata) {
-    pcli_t ctx = {.userdata = userdata};
-    for (usize i = 0; i < count; i++) {
-        if (opt[i].kind == PCLI_INVALID) continue;
+void pcli_push_id_to_map(pcli_command_id_map_t map[static 1], usize hash, usize id, pcli_command_t *command) {
+    map->num_elements++;
+    auto node = map->buckets[hash%32];
+    pcli_command_id_map_node_t *new_node = pallocate(sizeof *node);
+    *new_node = (pcli_command_id_map_node_t){};
+    new_node->id = id;
+    new_node->hash = hash;
+    new_node->value = command;
+    if (node) {
+        while (node->next) node = node->next;
+        node->next = new_node;
+    } else {
+        map->buckets[hash%32] = new_node;
+    }
+}
 
-        isize index = pcli_has_spelling(&ctx, opt[i].spelling);
-        if (index != -1) {
-            ctx.arguments[index].kind |= opt[i].kind;
+pcli_command_t *pcli_find_command_by_name(pcli_t ctx[static const 1], usize hash, pstring_t name) {
+    auto name_node = ctx->name_map.buckets[hash % 32];
+    while (name_node) {
+        if (name_node->hash == hash && pcmp_string(name_node->name, name))
+            return name_node->value;
+        name_node = name_node->next;
+    }
 
-            if (opt[i].kind & PCLI_TOGGLE) ctx.arguments[index].handlers[0] = opt[i].handler;
-            if (opt[i].kind & PCLI_EQUALS) ctx.arguments[index].handlers[1] = opt[i].handler;
-            if (opt[i].kind & PCLI_AFTER ) ctx.arguments[index].handlers[2] = opt[i].handler;
-            if (opt[i].kind & PCLI_NEXT  ) ctx.arguments[index].handlers[3] = opt[i].handler;
-            if (opt[i].kind & PCLI_MS    ) ctx.arguments[index].handlers[4] = opt[i].handler;
-        } else {
-            struct pcli_internal_argument_t arg = {
-                .spelling = opt[i].spelling,
-                .kind     = opt[i].kind,
-            };
-            
-            if (opt[i].kind & PCLI_TOGGLE) arg.handlers[0] = opt[i].handler;
-            if (opt[i].kind & PCLI_EQUALS) arg.handlers[1] = opt[i].handler;
-            if (opt[i].kind & PCLI_AFTER ) arg.handlers[2] = opt[i].handler;
-            if (opt[i].kind & PCLI_NEXT  ) arg.handlers[3] = opt[i].handler;
-            if (opt[i].kind & PCLI_MS    ) arg.handlers[4] = opt[i].handler;
-            
-            if (ctx.arguments) {
-                struct pcli_internal_argument_t *position = ctx.arguments;
-                while (position->spelling.length >= arg.spelling.length) {
-                    if (position == psb_end(ctx.arguments) - 1) break;
-                    position++;
+    return nullptr;
+}
+
+pcli_command_t *pcli_find_command_by_id(pcli_t ctx[static const 1], usize hash, usize id) {
+    auto id_node = ctx->id_map.buckets[hash % 32];
+    while (id_node) {
+        if (id_node->hash == hash && id_node->id == id)
+            return id_node->value;
+        id_node = id_node->next;
+    }
+
+    return nullptr;
+}
+
+pcli_command_t *pcli_has_command(pcli_t ctx[static const 1], usize name_hash, usize id_hash, pstring_t name, usize id) {
+    auto name_node = ctx->name_map.buckets[name_hash % 32];
+    while (name_node) {
+        if (name_node->hash == name_hash && pcmp_string(name_node->name, name))
+            return name_node->value;
+        name_node = name_node->next;
+    }
+    
+    auto id_node = ctx->id_map.buckets[id_hash % 32];
+    while (id_node) {
+        if (id_node->hash == id_hash && id_node->id == id)
+            return id_node->value;
+        id_node = id_node->next;
+    }
+
+    return nullptr;
+}
+
+void pcli_push_command(pcli_t ctx[static const 1], pstring_t name, usize id) {
+    if (name.length == 0 || name.c_str == nullptr) {
+        pcli_command_t new_command = {};
+        new_command.id = id;
+        new_command.name = name;
+        pcli_command_t *command = psb_pushback(ctx->commands, new_command);
+        usize hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+        pcli_push_id_to_map(&ctx->id_map, hash, id, command);
+        return;
+    }
+
+    if (name.length > plimits(isize).max) {
+        panic("length of command name was greater than %llu", plimits(isize).max);
+    }
+
+    usize name_hash = phash(name.c_str, (isize)name.length, PCLI_GLOBALS.seed); 
+    usize id_hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+
+    pcli_command_t *command = pcli_has_command(ctx, name_hash, id_hash, name, id);
+    if (command) {
+        if (pcmp_string(command->name, name)) 
+            return; // if the name matches this command was added twice, and we don't need to reprocess it
+        // if not then this is an alias
+        // add it to the alias array, and also to the name map
+        psb_pushback(command->aliases, name);
+        pcli_push_name_to_map(&ctx->name_map, name_hash, name, command);
+    } else {
+        pcli_command_t new_command = {};
+        new_command.id = id;
+        new_command.name = name;
+        command = psb_pushback(ctx->commands, new_command);
+        pcli_push_name_to_map(&ctx->name_map, name_hash, name, command);
+        pcli_push_id_to_map(&ctx->id_map, id_hash, id, command);
+    }
+}
+
+usize pcli_calculate_data_size(pcli_command_t *command) {
+    // we don't need to care about alignment as all of these are aligned by pointer size
+    static_assert(_Alignof(double)    == _Alignof(void*));
+    static_assert(_Alignof(char *)    == _Alignof(void*));
+    static_assert(_Alignof(pstring_t) == _Alignof(void*));
+    static_assert(_Alignof(isize)     == _Alignof(void*));
+
+    usize size = sizeof size;
+    psb_foreach(command->data, data) {
+      switch (*data) {
+      case PCLI_DATA_NONE: 
+      case PCLI_DATA_TOGGLE: 
+          break;
+      case PCLI_DATA_STRING:    size += sizeof(pstring_t); break;
+      case PCLI_DATA_STRING_C:  size += sizeof(char *);    break;
+      case PCLI_DATA_NUMBER:    size += sizeof(double);    break;
+      case PCLI_DATA_INT:       size += sizeof(isize);     break;
+        break;
+      }
+    }
+
+    return size;
+}
+
+void pcli_push_command_data(pcli_t ctx[static const 1], usize id, pcli_command_data_t data) {
+    usize hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+    pcli_command_t *command = pcli_find_command_by_id(ctx, hash, id);
+    if (!command) return;
+    psb_pushback(command->data, data);
+    usize data_size = pcli_calculate_data_size(command);
+    if (data_size > ctx->max_data_size) ctx->max_data_size = data_size;
+}
+
+void pcli_set_basecommand(pcli_t ctx[static const 1], usize id) {
+    usize hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+    pcli_command_t *command = pcli_find_command_by_id(ctx, hash, id);
+
+    ctx->has_basecommand = command != nullptr;
+    ctx->basecommand_id = id;
+}
+
+void pcli_set_help(pcli_t ctx[static const 1], usize id) {
+    usize hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+    pcli_command_t *command = pcli_find_command_by_id(ctx, hash, id);
+
+    ctx->has_help = command != nullptr;
+    ctx->help_id  = id;
+}
+
+void pcli_set_version(pcli_t ctx[static const 1], usize id) {
+    usize hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+    pcli_command_t *command = pcli_find_command_by_id(ctx, hash, id);
+
+    ctx->has_version = command != nullptr;
+    ctx->version_id  = id;
+}
+
+void pcli_set_short_version(pcli_t ctx[static const 1], usize id) {
+    usize hash   = phash(&id, sizeof id, PCLI_GLOBALS.seed); 
+    pcli_command_t *command = pcli_find_command_by_id(ctx, hash, id);
+
+    ctx->has_short_version = command != nullptr;
+    ctx->short_version_id  = id;
+}
+
+void pcli_parse_data(usize argc, char *argv[argc], pcli_command_t cmd[static 1], u8 *buffer) {
+    psb_foreach(cmd->data, data) {
+
+        switch (*data) {
+        case PCLI_DATA_NONE: 
+        case PCLI_DATA_TOGGLE: break;
+        case PCLI_DATA_STRING:   {
+                pstring_t *out = (pstring_t*)buffer;
+                if (argc) {
+                    out->c_str = argv[0];
+                    out->length = strlen(argv[0]);
+                    argc--; argv--;
+                } else {
+                    out->c_str = nullptr;
+                    out->length = 0;
                 }
-                if (position == psb_end(ctx.arguments) - 1)
-                     psb_pushback(ctx.arguments, arg);
-                else psb_insert(ctx.arguments, position, arg);
-            } else   psb_pushback(ctx.arguments, arg);
+                buffer = (u8*)(out + 1);
+            }
+        case PCLI_DATA_STRING_C: {
+                char **out = (char**)buffer;
+                if (argc) {
+                    *out = argv[0];
+                    argc--; argv--;
+                } else {
+                    *out = nullptr;
+                }
+                buffer = (u8*)(out + 1);
+            }
+        case PCLI_DATA_NUMBER: {
+                double *out = (double*)buffer;
+                if (argc) {
+                    *out = strtod(argv[0], &(char*){});
+                    argc--; argv--;
+                } else {
+                    *out = 0.0;
+                }
+                buffer = (u8*)(out + 1);
+            }  
+        case PCLI_DATA_INT: {
+                isize *out = (isize*)buffer;
+                if (argc) {
+                    *out = strtoll(argv[0], &(char*){}, 10);
+                    argc--; argv--;
+                } else {
+                    *out = 0;
+                }
+                buffer = (u8*)(out + 1);
+            }
+          break;
         }
     }
-    return ctx;
 }
 
-isize pcli_has_spelling(pcli_t *ctx, pstring_t str) {
-    psb_foreach(ctx->arguments, arg) {
-        if (pcmp_string(arg->spelling, str)) return arg - ctx->arguments;
-    }
-    return -1;
-}
-
-void pcli_run(pcli_t *ctx, int argc, const char *argv[argc]) {
-    for (int i = 1; i < argc; i++) {
-        usize length = strlen(argv[i]);
-        pstring_t arg_text = { length, (void*)argv[i] };
-        bool handler_result = true;
-
-        psb_foreach(ctx->arguments) {
-            pstring_t spelling = it->spelling;
-            // we have already sorted this to be longest string first
-            // so we already know that if won't be longer than the argument 
-            // after this one, so we stop iterating and go to the next arg
-            if (length < spelling.length) continue;
-            arg_text.length = spelling.length;
-
-            if (pcmp_string(arg_text, spelling)) {
-                if (spelling.length < length) {
-                    char *ms_test = arg_text.c_str + spelling.length;
-                    if ((it->kind & PCLI_MS) && (*ms_test == ':')) {
-                        arg_text.length  = length - (spelling.length + 1);
-                        arg_text.c_str  += spelling.length + 1;
-                        handler_result = it->handlers[PCLI_MS_INDEX](ctx, ctx->userdata, arg_text);
-                    }
-                    else if ((it->kind & PCLI_EQUALS) && (*ms_test == '=')) { 
-                        arg_text.length  = length - (spelling.length + 1);
-                        arg_text.c_str  += spelling.length + 1;
-                        handler_result = it->handlers[PCLI_EQUALS_INDEX](ctx, ctx->userdata, arg_text);
-                    } else {
-                        if (!(it->kind & PCLI_AFTER)) break;
-                        arg_text.length  = length - spelling.length;
-                        arg_text.c_str  += spelling.length;
-                        handler_result = it->handlers[PCLI_AFTER_INDEX](ctx, ctx->userdata, arg_text);
-                    }
-                    break;
-                } else {
-                    if ((i + 1 >= argc) || (it->kind & PCLI_TOGGLE)) {
-                        assert(it->kind & PCLI_TOGGLE);
-                        arg_text = pstring(NULL, 0);
-                        handler_result = it->handlers[PCLI_TOGGLE_INDEX](ctx, ctx->userdata, arg_text);
-                    }
-                    else if (it->kind & PCLI_NEXT) {
-                        arg_text = pstring((void*)argv[i+1], strlen(argv[i+1]));
-                        handler_result = it->handlers[PCLI_NEXT_INDEX](ctx, ctx->userdata, arg_text);
-                    } else {
-                        assert(false);
-                    }
-                    break;
-                }
+void pcli_process_command(usize argc, char *argv[argc], pcli_command_t cmd[static 1], u8 *buffer) {
+    if (argc) {
+        pstring_t current_word = pstring(*argv, strlen(*argv));
+        //TODO(Paw): check if (isize)current_word is save before continuing
+        usize hash   = phash(current_word.c_str, (isize)current_word.length, PCLI_GLOBALS.seed); 
+        
+        psb_foreach(cmd->sub_commands, sub_command) {
+            if (sub_command->hash == hash && pcmp_string(sub_command->name, current_word)) {
+                pcli_process_command(argc--, argv++, sub_command, buffer);
+                return;
             }
 
-            arg_text.length = length;
-            if (!handler_result) return;
+            psb_foreach(sub_command->aliases, alias) {
+                if (pcmp_string(*alias, current_word)) {
+                    pcli_process_command(argc--, argv++, sub_command, buffer);
+                    return;
+                }
+            }
         }
 
+        //TODO(Paw): add flags support
+
+
+        usize *id = (usize*)buffer;
+        *(usize*)buffer = cmd->id;
+        buffer = (u8*)(id + 1);
+        pcli_parse_data(argc, argv, cmd, buffer);
+        return;
     }
+
+    usize *id = (usize*)buffer;
+    *(usize*)buffer = cmd->id;
+    buffer = (u8*)(id + 1);
+} 
+
+void *pcli_run(pcli_t ctx[static const 1], int argc, char *argv[]) {
+    PCLI_GLOBALS.argc = argc;
+    PCLI_GLOBALS.argv = argv;
+    if (argc < 1) return nullptr;
+    argc--; argv++;
+
+    ctx->data_buffer = pzero_allocate(ctx->max_data_size + 1);
+
+    usize len = argc == 0 ? 0 : strlen(*argv);
+    pstring_t first_word = pstring(*argv, len);
+
+    if (pcmp_string(first_word, pcreate_const_string("--version")) && (ctx->has_version || ctx->has_short_version)) {
+        pcli_command_t *command = nullptr;
+        if (ctx->has_version) {
+            usize hash   = phash(&ctx->version_id, sizeof ctx->version_id, PCLI_GLOBALS.seed); 
+            command = pcli_find_command_by_id(ctx, hash, ctx->version_id);
+        } else {
+            usize hash   = phash(&ctx->short_version_id, sizeof ctx->short_version_id, PCLI_GLOBALS.seed); 
+            command = pcli_find_command_by_id(ctx, hash, ctx->short_version_id);
+        }
+        if (command == nullptr) return nullptr;
+
+        argc--; argv++;
+        pcli_process_command(argc, argv, command, ctx->data_buffer);
+        return (void*)ctx->data_buffer;
+    }
+    
+    if (pcmp_string(first_word, pcreate_const_string("-v")) && (ctx->has_version || ctx->has_short_version)) {
+        pcli_command_t *command = nullptr;
+        if (ctx->has_short_version) {
+            usize hash   = phash(&ctx->short_version_id, sizeof ctx->short_version_id, PCLI_GLOBALS.seed); 
+            command = pcli_find_command_by_id(ctx, hash, ctx->short_version_id);
+        } else {
+            usize hash   = phash(&ctx->version_id, sizeof ctx->version_id, PCLI_GLOBALS.seed); 
+            command = pcli_find_command_by_id(ctx, hash, ctx->version_id);
+        }
+        if (command == nullptr) return nullptr;
+
+        argc--; argv++;
+        pcli_process_command(argc, argv, command, ctx->data_buffer);
+        return (void*)ctx->data_buffer;
+    }
+    
+    if ((pcmp_string(first_word, pcreate_const_string("--help"))
+         || pcmp_string(first_word, pcreate_const_string("-help"))
+         || pcmp_string(first_word, pcreate_const_string("-?"))
+         || pcmp_string(first_word, pcreate_const_string("/?"))
+        ) && ctx->has_help)
+    {
+        usize hash   = phash(&ctx->help_id, sizeof ctx->help_id, PCLI_GLOBALS.seed); 
+        pcli_command_t *command = pcli_find_command_by_id(ctx, hash, ctx->help_id);
+        if (command == nullptr) return nullptr;
+
+        argc--; argv++;
+        pcli_process_command(argc, argv, command, ctx->data_buffer);
+        return (void*)ctx->data_buffer;
+    }
+
+    if (argc == 0 && ctx->has_basecommand) {
+        usize hash   = phash(&ctx->basecommand_id, sizeof ctx->basecommand_id, PCLI_GLOBALS.seed); 
+        pcli_command_t *command = pcli_find_command_by_id(ctx, hash, ctx->basecommand_id);
+        if (command == nullptr) return nullptr;
+
+        pcli_process_command(argc, argv, command, ctx->data_buffer);
+        return (void*)ctx->data_buffer;
+    }
+
+    usize hash   = phash(first_word.c_str, (isize)first_word.length, PCLI_GLOBALS.seed); 
+    pcli_command_t *command = pcli_find_command_by_name(ctx, hash, first_word);
+    if (command) {
+        argc--; argv++;
+        pcli_process_command(argc, argv, command, ctx->data_buffer);
+        return (void*)ctx->data_buffer;
+    }
+
+    if (ctx->has_basecommand) {
+        usize hash   = phash(&ctx->basecommand_id, sizeof ctx->basecommand_id, PCLI_GLOBALS.seed); 
+        pcli_command_t *command = pcli_find_command_by_id(ctx, hash, ctx->basecommand_id);
+        if (command != nullptr && psb_length(command->data) == 0) {
+            pcli_process_command(argc, argv, command, ctx->data_buffer);
+            return (void*)ctx->data_buffer;
+        }
+    }
+
+    if (ctx->has_help) {
+        usize hash   = phash(&ctx->help_id, sizeof ctx->help_id, PCLI_GLOBALS.seed); 
+        pcli_command_t *command = pcli_find_command_by_id(ctx, hash, ctx->help_id);
+        if (command == nullptr) return nullptr;
+        
+        pcli_process_command(argc, argv, command, ctx->data_buffer);
+        return (void*)ctx->data_buffer;
+    }
+
+    return nullptr;
 }
 
-void pcli_show_help(pcli_t *, int (*printer)(const char*, ...)) {
-    assert(false);
-}
-
-void pcli_show_verbose_help(pcli_t *, pstring_t argument, int (*printer)(const char*, ...)) {
-    (void)printer;
-    (void)argument;
-    assert(false);
-}
-
-void pcli_free(pcli_t *ctx) {
-    if (ctx->arguments) psb_free(ctx->arguments);
+void pcli_free(pcli_t *) {
+    panic("not implemented");
 }
 
